5ac67d19a2f120b67e528517a9e7dddf8a6adf85
Li Peihao <1120921822@qq.com>
2019-03-23 17:13:59 +0800

    finish DBUtil

--- BackCode/MeetingManager/src/com/dao/LoginDao.java
+++ BackCode/MeetingManager/src/com/dao/LoginDao.java
@@ -1,26 +1,48 @@
 package com.dao;
-import oracle.jdbc.driver.OracleDriver;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.DBUtil.DBUtil;
+import java.sql.*;
 
 public class LoginDao {
-    Connection connection = null;
-    Driver driver = null;
-    public boolean loginCheck(String account,String password) throws SQLException{
+    private Connection connection = null;
+    private PreparedStatement prestatement = null;
+    private ResultSet resultSet = null;
+    public boolean loginCheck(String account,String password,String athority){
         try{
-            //driver = new OracleDriver();
-            //DriverManager.deregisterDriver(driver);
-            Class.forName("oracle.jdbc.OracleDriver");
-            connection = DriverManager.getConnection("jdbc:oracle:@thin:@localhost:1521:XE","practiser","practiser");
-            //System.out.println(connection);
-
-        }catch (ClassNotFoundException e){
-
+            connection = DBUtil.getConnection();
+            connection.setAutoCommit(false);
+            prestatement = connection.prepareStatement("select * from account where account = ?");
+            prestatement.setString(1,account);
+            resultSet = prestatement.executeQuery();
+            if(resultSet.next()){
+               if(password == resultSet.getString("password")&& athority==resultSet.getString("athority")){
+                   connection.commit();
+                   return true;
+               }
+            }
+            return false;
+        }catch (SQLException e){
+            try {
+                if (connection != null) connection.rollback();
+            }catch (SQLException e1){
+                e1.printStackTrace();
+            }finally {
+                return false;
+            }
+        }finally {
+            try {
+                if(resultSet!=null) resultSet.close();
+                if(prestatement!=null) prestatement.close();
+                if(connection!=null) connection.close();
+            }catch (SQLException e2){
+                e2.printStackTrace();
+            }
         }
-        return true;
+    }
+
+    public static void main(String[] args){
+        LoginDao textDao = new LoginDao();
+        //textDao.loginCheck(" "," ");
     }
 
 }
--- BackCode/MeetingManager/src/com/servlet/LoginServlet.java
+++ BackCode/MeetingManager/src/com/servlet/LoginServlet.java
@@ -5,7 +5,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 
 @WebServlet(name = "LoginServlet")
 public class LoginServlet extends HttpServlet {
@@ -14,6 +17,14 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter out = response.getWriter();
+
+        BufferedReader reader = request.getReader();
+        String content = reader.readLine();
+
 
     }
 }
